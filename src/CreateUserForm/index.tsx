af6de34b47ad21a946bd73e6ae165b90141606e2
samuelisch <samuelchanwk@hotmail.com>
2022-04-26 23:49:42 +0800

    added login functionality to applicaiton with login form

--- src/CreateUserForm/index.tsx
+++ src/CreateUserForm/index.tsx
@@ -1,55 +1,81 @@
 import React, { useState } from 'react';
 import Button from '../assets/Button';
+import loginCalls from '../serverCalls/login';
 import userCalls from '../serverCalls/users';
 
 const CreateUserForm = () => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [location, setLocation] = useState<string>('');
+  const [createUsername, setCreateUsername] = useState<string>('');
+  const [createPassword, setCreatePassword] = useState<string>('');
+  const [loginUsername, setLoginUsername] = useState<string>('');
+  const [loginPassword, setLoginPassword] = useState<string>('');
 
   const createNewUser = async (e: React.FormEvent) => {
     e.preventDefault();
-    const newUser = { username, password, location };
+    const newUser = { username: createUsername, password: createPassword };
     try {
       const createdUser = await userCalls.createUser(newUser);
       console.log(createdUser);
 
       // reset form
-      setUsername('');
-      setPassword('');
-      setLocation('');
+      setCreateUsername('');
+      setCreatePassword('');
     } catch (error: any) {
       if (error.response) {
-        console.log(error.response.data)
+        console.log(error.response.data);
+      }
+    }
+  };
+
+  const loginUser = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const credentials = { username: loginUsername, password: loginPassword };
+    try {
+      const tokens = await loginCalls.loginUser(credentials);
+      console.log(tokens);
+    } catch (error: any) {
+      if (error.response) {
+        console.log(error.response.data);
       }
     }
   };
 
   return (
-    <form onSubmit={createNewUser}>
-      <input
-        aria-label="usernameInput"
-        type="text"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-        placeholder="username"
-      />
-      <input
-        aria-label="passwordInput"
-        type="text"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        placeholder="password"
-      />
-      <input
-        aria-label="locationInput"
-        type="text"
-        value={location}
-        onChange={e => setLocation(e.target.value)}
-        placeholder="location"
-      />
-      <Button className="signupSubmit" type="submit" text="Create user" />
-    </form>
+    <>
+      <form onSubmit={createNewUser}>
+        <input
+          aria-label="usernameInput"
+          type="text"
+          value={createUsername}
+          onChange={e => setCreateUsername(e.target.value)}
+          placeholder="username"
+        />
+        <input
+          aria-label="passwordInput"
+          type="text"
+          value={createPassword}
+          onChange={e => setCreatePassword(e.target.value)}
+          placeholder="password"
+        />
+        <Button className="signupSubmit" type="submit" text="Create user" />
+      </form>
+      <form onSubmit={loginUser}>
+        <input
+          aria-label="usernameInput"
+          type="text"
+          value={loginUsername}
+          onChange={e => setLoginUsername(e.target.value)}
+          placeholder="username"
+        />
+        <input
+          aria-label="passwordInput"
+          type="text"
+          value={loginPassword}
+          onChange={e => setLoginPassword(e.target.value)}
+          placeholder="password"
+        />
+        <Button className="loginSubmit" type="submit" text="Login user" />
+      </form>
+    </>
   );
 };
 
---
+++ src/serverCalls/login.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosResponse } from 'axios';
+import { UserType } from '../utils/types';
+
+const baseUrl = 'http://localhost:8000/api/token/';
+
+const loginUser = async (credentials: UserType): Promise<AxiosResponse> => {
+  const response = await axios.post(baseUrl, credentials);
+  return response.data
+}
+
+const loginCalls = {
+  loginUser
+}
+
+export default loginCalls
