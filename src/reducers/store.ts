839ae92a44ae7aba4912a0e4e6971a2dd97ab17c
samuelisch <samuelchanwk@hotmail.com>
2022-04-29 17:05:00 +0800

    switched to typed hooks for redux

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,16 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import usersReducer from './usersSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    users: usersReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = typeof store.dispatch;
