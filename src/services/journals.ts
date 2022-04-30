4c4850f79347ad71db03a12765c368b477e3177c
samuelisch <samuelchanwk@hotmail.com>
2022-04-30 19:43:48 +0800

    added get userl by username, edited naming of journals calls

--- src/services/journals.ts
+++ src/services/journals.ts
@@ -2,13 +2,13 @@ import axios, { AxiosResponse } from 'axios';
 
 const baseUrl = 'http://localhost:8000/api/journals';
 
-const getHabits = async (): Promise<AxiosResponse> => {
-  const response = await axios.get(`${baseUrl}/`);
+const getJournals = async (): Promise<AxiosResponse> => {
+  const response = await axios.get(`${baseUrl}/journal-list/`);
   return response.data;
 };
 
 const habitCalls = {
-  getHabits,
+  getJournals,
 };
 
 export default habitCalls;
--- src/services/users.ts
+++ src/services/users.ts
@@ -1,5 +1,7 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import { UserSchema } from '../reducers/usersSlice';
 import { UserType } from '../utils/types';
+import { token } from './login';
 
 const baseUrl = 'http://localhost:8000/api/users';
 
@@ -8,6 +10,15 @@ const getUsers = async (): Promise<AxiosResponse> => {
   return response.data;
 };
 
+const getUserByUsername = async (username: string): Promise<AxiosResponse | UserSchema> => {
+  const config: AxiosRequestConfig = {
+    headers: { 'Authorization': token },
+  }
+
+  const response = await axios.get(`${baseUrl}/user-detail/${username}`, config);
+  return response.data
+}
+
 const createUser = async (userObj: UserType): Promise<AxiosResponse> => {
   const response = await axios.post(`${baseUrl}/user-create/`, userObj);
   return response.data;
@@ -15,6 +26,7 @@ const createUser = async (userObj: UserType): Promise<AxiosResponse> => {
 
 const userCalls = {
   getUsers,
+  getUserByUsername,
   createUser,
 };
 
