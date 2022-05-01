bcc7f169b3a9f77157fa948d2d6972f00aca85e8
samuelisch <samuelchanwk@hotmail.com>
2022-05-01 16:39:51 +0800

    Created JournalType and config for creating journal

--- src/services/journals.ts
+++ src/services/journals.ts
@@ -1,14 +1,25 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { token } from './login';
 
 const baseUrl = 'http://localhost:8000/api/journals';
+
+export interface JournalType {
+  title: string;
+  content: string;
+  user: string | number;
+}
 
 const getJournals = async (): Promise<AxiosResponse> => {
   const response = await axios.get(`${baseUrl}/journal-list/`);
   return response.data;
 };
 
-const createJournal = async (): Promise<AxiosResponse> => {
-  const response = await axios.get(`${baseUrl}/journal-create/`);
+const createJournal = async (journalObj: JournalType): Promise<AxiosResponse> => {
+  const config: AxiosRequestConfig = {
+    headers: { Authorization: token },
+  };
+
+  const response = await axios.post(`${baseUrl}/journal-create/`, journalObj, config);
   return response.data;
 }
 
