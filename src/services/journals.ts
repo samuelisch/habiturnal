fb7e9e1803260477afe8d42357ade201447bd859
samuelisch <samuelchanwk@hotmail.com>
2022-05-02 13:22:11 +0800

    added journaltype and renamed journalinputtype

--- src/services/journals.ts
+++ src/services/journals.ts
@@ -3,25 +3,33 @@ import { token } from './login';
 
 const baseUrl = 'http://localhost:8000/api/journals';
 
-export interface JournalType {
+export interface JournalInputType {
   title: string;
   content: string;
   user: string | number;
 }
 
-const getJournals = async (): Promise<AxiosResponse> => {
+export interface JournalType {
+  id: string | number;
+  title: string;
+  content: string;
+  user: string | number;
+  created_date: string | number | Date;
+}
+
+const getJournals = async (): Promise<AxiosResponse | JournalType[]> => {
   const response = await axios.get(`${baseUrl}/journal-list/`);
   return response.data;
 };
 
-const createJournal = async (journalObj: JournalType): Promise<AxiosResponse> => {
+const createJournal = async (journalObj: JournalInputType): Promise<AxiosResponse> => {
   const config: AxiosRequestConfig = {
     headers: { Authorization: token },
   };
 
   const response = await axios.post(`${baseUrl}/journal-create/`, journalObj, config);
   return response.data;
-}
+};
 
 const journalCalls = {
   getJournals,
