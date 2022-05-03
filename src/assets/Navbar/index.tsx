e4e2ab4b349493f461b3f7216825ef1a315ef556
samuelisch <samuelchanwk@hotmail.com>
2022-05-03 23:41:22 +0800

    style side navbar for mobile view

---
+++ src/assets/Navbar/index.tsx
@@ -0,0 +1,54 @@
+import styles from './Navbar.module.scss';
+import { BsFillJournalBookmarkFill } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi';
+import { FiSettings } from 'react-icons/fi';
+import { GiBookshelf } from 'react-icons/gi';
+import { CgProfile } from 'react-icons/cg';
+import { SiLivejournal } from 'react-icons/si';
+import { useDispatch } from 'react-redux';
+import { invalidate } from '../../reducers/authSlice';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../App/ProtectedContainer';
+
+
+const Navbar = () => {
+  const user = useContext(UserContext);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logout = () => {
+    dispatch(invalidate());
+    navigate('/login');
+  };
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Top}>
+        <div className={styles.IconHome} onClick={() => navigate('/home')}>
+          <GiBookshelf size='25px' />
+        </div>
+        <div className={styles.Icon} onClick={() => navigate(`/user/${user.id}`)}>
+          <CgProfile size='25px' />
+        </div>
+        <div className={styles.Icon} onClick={() => navigate('/journals/create')}>
+          <SiLivejournal size='25px' />
+        </div>
+      </div>
+      <div className={styles.Bottom}>
+        <div className={styles.Icon}>
+          <FiSettings size='25px' />
+        </div>
+        <div className={styles.Icon} onClick={logout}>
+          <BiLogOut size='25px' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
