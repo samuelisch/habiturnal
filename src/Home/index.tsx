353d29946a18718d1a4b4a3124a8acb92e1cb9b8
samuelisch <samuelchanwk@hotmail.com>
2022-05-03 23:41:53 +0800

    updated styling for home component, scrollbar

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,9 +1,7 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../App/ProtectedContainer';
-import Button from '../assets/Button';
 import JournalsList from '../Journals/JournalsList';
-import { invalidate } from '../reducers/authSlice';
 import { useAppDispatch } from '../reducers/hooks';
 import { fetchJournals } from '../reducers/journalsSlice';
 
@@ -16,20 +14,13 @@ const Home = () => {
     (async () => {
       dispatch(fetchJournals());
     })();
-  }, [dispatch]);
-
-  const logout = () => {
-    dispatch(invalidate());
-    navigate('/login');
-  };
+  }, []);
 
   return (
-    <div>
-      {user ? <h1> Welcome, <span onClick={() => navigate(`/user/${user.id}`)}>{user.username}!</span></h1> : ''}
-      <Button className="logoutBtn" type="button" text="logout" clickHandler={logout} />
-      <Button className="createJournal" type="button" text="create new" clickHandler={() => navigate('/journals/create')} />
+    <>
+      {/* add tab here to see all or near with flag of location */}
       <JournalsList />
-    </div>
+    </>
   );
 };
 
