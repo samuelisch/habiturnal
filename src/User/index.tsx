66b5bc41abb00fc807a4004e07eccbfd3f0f9774
samuelisch <samuelchanwk@hotmail.com>
2022-05-04 13:45:00 +0800

    Styled base user file

--- src/User/index.tsx
+++ src/User/index.tsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import Button from "../assets/Button";
-import Loading from "../assets/Loading";
-import { UserSchema } from "../reducers/usersSlice";
-import userCalls from "../services/users";
-
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Button from '../assets/Button';
+import Loading from '../assets/Loading';
+import JournalsList from '../Journals/JournalsList';
+import { UserSchema } from '../reducers/usersSlice';
+import userCalls from '../services/users';
+import styles from './User.module.scss';
 
 const User = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [selectedUser, setSelectedUser] = useState<UserSchema | null>(null)
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [selectedUser, setSelectedUser] = useState<UserSchema | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     let fetching = true;
@@ -31,30 +32,28 @@ const User = () => {
 
     return () => {
       fetching = false;
-    }
-  }, [id])
+    };
+  }, [id]);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (!selectedUser) {
-    return <h1>No user found!</h1>
+    return <h1>No user found!</h1>;
   }
 
   return (
     <div>
-      <h1>User: {selectedUser.username}</h1>
-      <p>Location: {selectedUser.location}</p>
-      <p>Joined: {new Date(selectedUser.date_joined).getTime()}</p>
-      <Button
-          className="goBack"
-          type="button"
-          text="Back"
-          clickHandler={() => navigate(-1)}
-        />
+      <div className={styles.UserMeta}>
+        <h1>User: {selectedUser.username}</h1>
+        <p>Location: {selectedUser.location}</p>
+        <p>Joined: {new Date(selectedUser.date_joined).getTime()}</p>
+        <Button className="goBack" type="button" text="Back" clickHandler={() => navigate(-1)} />
+      </div>
+      <JournalsList />
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
