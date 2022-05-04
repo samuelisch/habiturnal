3c4cb5edbeae75320b2cfba9d39238d64f496df3
samuelisch <samuelchanwk@hotmail.com>
2022-05-04 13:46:44 +0800

    moved useEffect call to JournalList component

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from 'react';
 import JournalsList from '../Journals/JournalsList';
-import { useAppDispatch } from '../reducers/hooks';
-import { fetchJournals } from '../reducers/journalsSlice';
 
 import styles from './Home.module.scss';
 
 const Home = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    (async () => {
-      dispatch(fetchJournals());
-    })();
-  }, []);
-
   return (
     <div className={styles.Container}>
       {/* add tab here to see all or near with flag of location */}
