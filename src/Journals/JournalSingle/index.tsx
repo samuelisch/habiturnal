94403242088e970ebd8d8e3c5ac850fecafe7f4f
samuelisch <samuelchanwk@hotmail.com>
2022-05-04 10:34:13 +0800

    style single journal component, save not working

--- src/Journals/JournalSingle/index.tsx
+++ src/Journals/JournalSingle/index.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import { JournalType } from '../../services/journals';
+import { calcReadTime } from '../../utils/utilfunc';
 import styles from './JournalSingle.module.scss';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa';
+import TimeAgo from '../../assets/TimeAgo';
 
 interface Props {
   journal: JournalType;
@@ -8,16 +11,41 @@ interface Props {
 
 const JournalSingle = ({ journal }: Props) => {
   const navigate = useNavigate();
+
   const openJournal = () => {
-    navigate(`/journals/view/${journal.id}`)
-  }
+    navigate(`/journals/view/${journal.id}`);
+  };
+
+  const cutPreview = (words: string) => {
+    return words.length > 100 ? `${words.slice(0, 100)} ...` : words;
+  };
 
   return (
-    <div className={styles.Container} onClick={openJournal}>
-      <h1>{journal.title}</h1>
-      <p>{journal.content}</p>
-      <div>{journal.owner}</div>
-      <span>{new Date(journal.created_date).getTime()}</span>
+    <div className={styles.Container}>
+      <div className={styles.Meta} onClick={() => navigate('')}>
+        <div>
+          <span className={styles.Author}>{journal.owner}</span>
+          <span className={styles.Divider}> - </span>
+        </div>
+        <div>
+          <TimeAgo timestamp={new Date(journal.created_date).toISOString()} />
+        </div>
+      </div>
+      <div className={styles.JournalLink} onClick={openJournal}>
+        <div className={styles.Title}>{journal.title}</div>
+        <div className={styles.Preview}>{cutPreview(journal.content)}</div>
+      </div>
+      <div className={styles.Actions}>
+        <div>
+          <span className={styles.ReadTime} onClick={openJournal}>
+            {calcReadTime(journal.content.length)} min read
+          </span>
+          <span className={styles.Divider}> - </span>
+        </div>
+        <div>
+          <FaRegBookmark className={styles.Like} size="15px" />
+        </div>
+      </div>
     </div>
   );
 };
