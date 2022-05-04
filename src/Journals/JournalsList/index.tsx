7a079f9f844634721ec8e061c2ff71b92925da85
samuelisch <samuelchanwk@hotmail.com>
2022-05-04 13:45:52 +0800

    added end of file text, filter for different lists

--- src/Journals/JournalsList/index.tsx
+++ src/Journals/JournalsList/index.tsx
@@ -1,10 +1,29 @@
-import { useAppSelector } from '../../reducers/hooks';
-import { selectAllJournals } from '../../reducers/journalsSlice';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../reducers/hooks';
+import { fetchJournals, selectAllJournals } from '../../reducers/journalsSlice';
 import { JournalType } from '../../services/journals';
 import JournalSingle from '../JournalSingle';
+import styles from './JournalsList.module.scss';
 
-const JournalsList = () => {
+interface Props {
+  filter?: string;
+}
+
+const JournalsList = ({ filter }: Props) => {
+  const dispatch = useAppDispatch();
   const allJournals = useAppSelector(selectAllJournals);
+
+  useEffect(() => {
+    (async () => {
+      if (filter === 'user') {
+        
+      }
+      else if (filter === 'saved') {}
+      else if (filter === 'near') {}
+      else {
+        await dispatch(fetchJournals());
+    }})();
+  }, [filter, dispatch]);
 
   const sortedJournals = [...allJournals].sort((a: JournalType, b: JournalType) => {
     return new Date(b.created_date).getTime() - new Date(a.created_date).getTime()
@@ -15,9 +34,13 @@ const JournalsList = () => {
   ))
 
   return (
+    // TODO: TABS FOR ALL / NEAR YOU
     <div>
       <h1>Journals List</h1>
       { displayJournals }
+      <div className={styles.End}>
+        <span className={styles.EndText}>You've reached the end.</span>
+      </div>
     </div>
   );
 };
