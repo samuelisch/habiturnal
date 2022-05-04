6b89cc48beffa5ac992e787aba308830f594a462
samuelisch <samuelchanwk@hotmail.com>
2022-05-04 15:54:19 +0800

    minor styling changes on user component

--- src/User/index.tsx
+++ src/User/index.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import Button from '../assets/Button';
+import { useParams } from 'react-router-dom';
 import Loading from '../assets/Loading';
 import JournalsList from '../Journals/JournalsList';
 import { UserSchema } from '../reducers/usersSlice';
 import userCalls from '../services/users';
 import styles from './User.module.scss';
+import ReactCountryFlag from 'react-country-flag';
+import { format, parseISO } from 'date-fns';
 
 const User = () => {
-  const navigate = useNavigate();
   const { id } = useParams();
   const [selectedUser, setSelectedUser] = useState<UserSchema | null>(null);
+  const [dateJoined, setDateJoined] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -35,6 +36,14 @@ const User = () => {
     };
   }, [id]);
 
+  useEffect(() => {
+    if (selectedUser) {
+      const date = parseISO(selectedUser.date_joined)
+      const formattedJoinedDate = format(date, 'MMMM R')
+      setDateJoined(formattedJoinedDate);
+    }
+  }, [selectedUser])
+
   if (isLoading) {
     return <Loading />;
   }
@@ -46,10 +55,17 @@ const User = () => {
   return (
     <div>
       <div className={styles.UserMeta}>
-        <h1>User: {selectedUser.username}</h1>
-        <p>Location: {selectedUser.location}</p>
-        <p>Joined: {new Date(selectedUser.date_joined).getTime()}</p>
-        <Button className="goBack" type="button" text="Back" clickHandler={() => navigate(-1)} />
+        <div className={styles.Details}>
+          <div className={styles.Username}>{selectedUser.username}</div>
+          <ReactCountryFlag
+            countryCode={selectedUser.location}
+            svg
+            style={{
+              fontSize: '2rem'
+            }}
+          />
+        </div>
+        <div className={styles.Date}>Joined: {}</div>
       </div>
       {/* // TODO: TABS FOR USER JOURNALS / SAVED JOURNALS - to change state and pass to journalsList */}
       <JournalsList filter='user' value={id} />
